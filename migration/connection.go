1a146d7a5e0363f7236d6a6417faa168a49a825e
LanternNassi <Lanternnassi@gmail.com>
2024-02-21 19:14:58 +0300

    Adding secrets

--- migration/connection.go
+++ migration/connection.go
@@ -1,14 +1,33 @@
 package main
 
 import (
-	"gorm.io/gorm"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+
 	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 // NewDB - Get gorm DB instance.
 func NewDB() (*gorm.DB, error) {
-	//dsn := "host=localhost user=postgres password=123456789 dbname=imscontroller port=5432 sslmode=disable TimeZone=UTC"
-	dsn := "host=102.134.147.233 user=tsaonokrtitbzytk password=4HLeh9LwP0aN+Dach0E1jt5I7K%N%5m7 dbname=edendsmwqscdihljjantklpo port=32761 sslmode=disable TimeZone=UTC"
+
+	err_env := godotenv.Load(".env")
+	if err_env != nil {
+		log.Fatalf("Error loading environment variables file")
+	}
+
+	dbport, err_conv := strconv.Atoi(os.Getenv("DBPORT"))
+
+	if err_conv != nil {
+		fmt.Println("Error converting string to int:", err_conv)
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s", os.Getenv("DBHOST"), os.Getenv("DBUSER"), os.Getenv("DBPASSWORD"), os.Getenv("DBNAME"), dbport, "disable")
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	return db, err
 }
